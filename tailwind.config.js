a6dd5a72b5e9c95a1aebb65a9269bf4daa07e50f
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-04 21:08:35 +0700

    chore: setup fonts, logo and favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,26 @@ module.exports = {
   ],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        danger: "#fc5185",
+        light: "#E6F1FF",
+        primary: "#7756fb",
+        "wire-primary": "#b6a4fc",
+        secondary: "#040918",
+        "light-secondary": "#0b1841",
+        "lightness-secondary": "#142145",
+        slate: "#8892B0",
+        "light-slate": "#A8B2D1",
+        "lightness-slate": "#CCD6F6",
+      },
+      fontFamily: {
+        mono: ["'Inconsolata'", "monospace"],
+        heading: ['"Druk Wide Web Bold"', "sans-serif"],
+        "heading-2": ["'Work Sans'", "sans-serif"],
+        body: ["'Space Grotesk'", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
