32a179a93824e7347b6f40275cf2a58a1831ad73
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-08 05:39:54 +0700

    refactor: move favicon configuration from file _App.tsx to file _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,8 +14,31 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@200;300;400;500;600;700;800;900&family=Space+Grotesk:wght@300;400;500;600;700&family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
           rel="stylesheet"
         />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest"></link>
+        <title>Loading... || Muhammad Akbar L</title>
       </Head>
       <body>
+        <div
+          id="page-preloader"
+          className=" fixed left-0 top-0 bottom-0 right-0 w-full h-screen bg-secondary border-1 z-[66666]  "
+        ></div>
         <Main />
         <NextScript />
       </body>
