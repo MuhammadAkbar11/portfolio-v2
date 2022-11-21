765105f61f6cc8be9b0458754548135849bcf0cf
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2022-11-21 11:27:59 +0700

    feat: Create a constants file, then add a constant color and constant navigation links.

---
+++ utils/constants.utils.ts
@@ -0,0 +1,26 @@
+export const COLORS = {
+  danger: "#fc5185",
+  light: "#E6F1FF",
+  primary: "#64ffda",
+  wirePrimary: "#B7FFEE",
+  cursorBgPrimary: "rgba(129, 229, 206, 0.25)",
+  secondary: "#040918",
+  lightSecondary: "#0b1841",
+  lightnessSecondary: "#142145",
+  slate: "#8892B0",
+  lightSlate: "#A8B2D1",
+  lightnessSlate: "#CCD6F6",
+};
+
+export const NAVLINKS = [
+  {
+    key: "#1",
+    name: "About",
+    url: "/about",
+  },
+  {
+    key: "#2",
+    name: "Work",
+    url: "/work",
+  },
+];
